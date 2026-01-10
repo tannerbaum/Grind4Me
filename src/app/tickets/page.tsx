a9bf1854aba231efcbc7f4065f3264aefcfff8cb
tannerbaum <tanner@storygrounds.com>
2026-01-10 17:58:51 -0500

    misc

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,21 +1,31 @@
 import Link from "next/link";
 import { initialTickets } from "@/data";
 
+const TICKET_ICONS = {
+  OPEN: "🟢",
+  DONE: "✅",
+};
+
 const TicketsPage = () => {
-    return <div>
-        {
-            initialTickets.map((ticket) => (
-                <div key={ticket.id}>
-                    <h3 className="text-xl font-bold">{ticket.title}</h3>
-                    <p>{ticket.content}</p>
-                    <p>Status: {ticket.status}</p>
-                    <Link href={`/tickets/${ticket.id}`} className="text-blue-500 hover:underline">
-                        View Ticket
-                    </Link>
-                </div>
-            ))
-        }
-    </div>;
-}
+  return (
+    <div>
+      {initialTickets.map((ticket) => (
+        <div key={ticket.id}>
+          <h3 className="text-xl font-bold">{ticket.title}</h3>
+          <p>{ticket.content}</p>
+          <p>
+            Status: {TICKET_ICONS[ticket.status]} {ticket.status}
+          </p>
+          <Link
+            href={`/tickets/${ticket.id}`}
+            className="text-blue-500 hover:underline"
+          >
+            View Ticket
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default TicketsPage;
--- src/data.ts
+++ src/data.ts
@@ -3,12 +3,12 @@ export const initialTickets = [
     id: "1",
     title: "Ticket 1",
     content: "This is the content of ticket 1",
-    status: "DONE",
+    status: "DONE" as const,
   },
   {
     id: "2",
     title: "Ticket 2",
     content: "This is the content of ticket 2",
-    status: "OPEN",
+    status: "OPEN" as const,
   },
 ];
