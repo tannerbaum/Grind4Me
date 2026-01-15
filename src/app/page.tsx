a6bc5c879f8ed39f5f3dc225cdf837e9bc779e6e
tannerbaum <tanner@storygrounds.com>
2026-01-15 16:30:42 -0500

    misc, add clsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h2 className="text-2xl">Home Page</h2>
-      <Link href="/tickets" className="text-blue-500 hover:underline">
-        View Tickets
-      </Link>
+    <div className="flex-1 flex flex-col gap-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
+        <p className="text-sm text-muted-foregound">Your place to start</p>
+      </div>
+      <div className="flex-1 flex flex-col items-center">
+        <Link href="/tickets" className="text-blue-500 hover:underline">
+          View Tickets
+        </Link>
+      </div>
     </div>
   );
 }
