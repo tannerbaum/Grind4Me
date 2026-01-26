1a3644c6d48a1763174695f43a9fdfaf3ac04f6e
tannerbaum <tanner@storygrounds.com>
2026-01-26 18:45:37 -0500

    upgrade to Next 16

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
+  reactCompiler: true,
+  // Would be interested, but doesn't play great with Prisma yet https://github.com/prisma/prisma/issues/28588
+  // cacheComponents: true,
 };
 
 export default nextConfig;
