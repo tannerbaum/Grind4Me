a035fca7dc7e6f0691b14a36709366eebf3aada7
tannerbaum <tanner@storygrounds.com>
2026-02-02 16:33:12 -0500

    put redirect toast in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/components/header";
 import { ThemeProvider } from "@/features/theme/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { RedirectToast } from "@/components/redirect-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
             {children}
           </main>
           <Toaster expand />
+          <RedirectToast />
         </ThemeProvider>
       </body>
     </html>
--- src/components/redirect-toast.tsx
+++ src/components/redirect-toast.tsx
@@ -4,8 +4,11 @@ import { useEffect } from "react";
 import { toast } from "sonner";
 import { consumeCookie } from "@/actions/cookies";
 import { COOKIE_KEYS } from "@/lib/constants";
+import { usePathname } from "next/navigation";
 
 const RedirectToast = () => {
+  const pathname = usePathname();
+
   useEffect(() => {
     const showCookieToast = async () => {
       const message = await consumeCookie(COOKIE_KEYS.TOAST);
@@ -16,7 +19,7 @@ const RedirectToast = () => {
     };
 
     showCookieToast();
-  }, []);
+  }, [pathname]);
 
   return null;
 };
